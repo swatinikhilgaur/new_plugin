98440914d619ecece3d333c1f155ed304e4ed11f
swati <php.swati@gmail.com>
2015-06-03 13:51:45 +0530

    Development complete

---
+++ new_plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package New Plugin
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: New Plugin
+Version: 1.0.0
+*/
+
+if(!defined('ABSPATH')) {
+  die('You are not allowed to call this page directly.');
+}
+
+/**
+ * Let's get started!
+ */
+class New_Plugin_Setup {
+  
+}
+
+/**
+ * Initialize
+ */
+new New_Plugin_Setup();
